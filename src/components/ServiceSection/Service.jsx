f4833063f98acc6f40726c44bf7c861ea4e573bc
Nelson877 <66440163+Nelson877@users.noreply.github.com>
2024-07-11 14:10:05 +0000

    new changes update

--- src/components/ServiceSection/Service.jsx
+++ src/components/ServiceSection/Service.jsx
@@ -7,7 +7,7 @@ const Service = () => {
         <div className="xl:w-[470px] mx-auto">
           <h2 className="heading text-center">Our Medical Services</h2>
           <p className="text__para text-center ">
-            Our medical services are designed to give affordable access to
+            Our medical services are designed to give affordable access <br />  to
             medical care for insured, uninsured and underinsuredd clients.
           </p>
         </div>
